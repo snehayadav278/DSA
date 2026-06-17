2d5290469c16b11addf51189c8c95c28f6c7bf7f
snehayadav278 <snehay851@gmail.com>
2026-06-17 13:54:25 +0530

    Reverse array using two-pointer approach

--- 01_Arrays/04_ReverseArray.cpp
+++ 01_Arrays/04_ReverseArray.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// Reverse array using two-pointer approach
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 void reverseArray(int arr[], int size){
     int start = 0;
     int end = size-1;
 
     while(start < end){
-        swap(arr[start] , arr[end]);
+        swap(arr[start] , arr[end]);  // swap elements
         start++;
         end--;
     }
@@ -16,8 +19,9 @@ int main(){
     int arr[] = {4, 3, 6, 8, 1, 9, 2};
     int size = 7;
 
-    reverseArray(arr, size);
+    reverseArray(arr, size);  // function call
 
+    // print reversed array
     for(int i=0; i<size ; i++){
         cout<< arr[i] <<" ";
     }
