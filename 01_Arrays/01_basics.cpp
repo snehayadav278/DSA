90db47f91409e2aa10c524e2166653a86871cdd8
snehayadav278 <snehay851@gmail.com>
2026-06-17 12:54:11 +0530

    Arrays basics and min/max element

---
+++ 01_Arrays/01_basics.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    // int marks[5] = {99 , 100 , 54 , 36 , 88};
+    // cout<< marks[0] << endl ;
+    // cout<< marks[1] << endl ;
+    // cout<< marks[2] << endl ;
+    // cout<< marks[3] << endl ;
+    // cout<< marks[4] << endl ;
+
+
+
+    //0 to size-1
+
+
+    // int size= 5 ;
+    // int marks[size];
+
+    //input
+    // for(int i =0 ; i<size ; i++){
+    //     cin >> marks[i];
+    // }
+
+    //cout << sizeof(marks) / sizeof(int) << endl;
+
+    //loops : 0 to size-1
+    // for(int i=0 ; i < size ; i++){
+    //     cout << marks[i] <<endl;
+    // }
+
+
+
+
+    // find smallest / largest in array 
+    // +infinity = INT_MAX
+
+    // int nums[] = { 5 , 15 , 22 , -15 , 34};
+    // int size = 5 ;
+    // int smallest = INT_MAX ;
+    // int largest = INT_MIN ;
+
+    // for(int i = 0 ; i < size ; i++){
+
+    //     // smallest = min(nums[i],smallest);
+
+    //     if(nums[i] < smallest){
+    //         smallest = nums[i];
+    //     }
+
+    //     largest = max(nums[i] , largest);
+    // }
+    // cout << "smallest = " << smallest << endl ;
+    // cout << "largest = " << largest << endl ;
+
+
+
+
+
+    return 0;
+}
