a135bc5c53922232065219694f4c880ce486e427
snehayadav278 <snehay851@gmail.com>
2026-06-17 17:06:58 +0530

    Learned vector basics

--- 01_Arrays/05_Vectors.cpp
+++ 01_Arrays/05_Vectors.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 int main(){
-    //vector<int>vec;
+
+    // Declaring an empty vector
+    // vector<int>vec;
 
 
-    //vector<int> vec = {1,2,3};
-    //cout<< vec[0];
+    // // vector initialization with values
+    // vector<int> vec = {1,2,3};
+    // cout<< vec[0];
 
 
+    // // vector of size 5 initialized with 0
     // vector<int> vec(5,0);
     // cout << vec[0] << endl;
     // cout << vec[1] << endl;
@@ -18,15 +22,19 @@ int main(){
     // cout << vec[4] << endl;
 
 
+    // vector of characters + range-based loop
     // vector<char> vec = {'a' , 'b', 'c', 'd', 'e'};
     // for(char val : vec){ //for each loop
     //     cout << val << endl;
     // }
 
 
+    // checking size of vector
     // vector<char> vec = {'a' , 'b', 'c', 'd', 'e'};
     // cout << "size = " << vec.size() << endl;
 
+
+    // adding elements using push_back()
     // vector<int> vec;
     // cout << "size = " << vec.size() << endl;
     // vec.push_back(15);
@@ -35,12 +43,13 @@ int main(){
     // vec.push_back(45);
     // cout << "after push_back , size = " << vec.size() << endl;
 
+    // removes last element
     // vec.pop_back(); //45
 
     // for(int val : vec){
     //     cout << val << endl;
     // }
-
+ 
     // cout << vec.front() << endl;
 
     //  cout << vec.back() << endl;
@@ -49,15 +58,19 @@ int main(){
     //  cout << vec.at(2) << endl;
 
     
-    vector<int> vec;
-    vec.push_back(0);
-    vec.push_back(1);
-    vec.push_back(2);
-    vec.push_back(3);
-    vec.push_back(4);
 
-    cout << vec.size() << endl;
-    cout << vec.capacity() << endl;
+    // Size vs Capacity
+    // vector<int> vec;
+    // vec.push_back(0);
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+
+    // cout << vec.size() << endl;
+    // cout << vec.capacity() << endl;
+
+
 
 
 
