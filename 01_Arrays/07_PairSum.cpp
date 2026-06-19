03c08b236980b0fddf378bf515ef8cfb0f062e23
snehayadav278 <snehay851@gmail.com>
2026-06-19 11:08:07 +0530

    Implemented Two Sum with brute force and optimized two-pointer for sorted array

--- 01_Arrays/07_PairSum.cpp
+++ 01_Arrays/07_PairSum.cpp
@@ -4,38 +4,72 @@ using namespace std;
 
 // Function to find indices of two numbers whose sum = target
 // Brute Force Approach (O(n^2))
-vector<int> pairSum(vector<int> nums , int target){
-    vector<int> ans;
-    int n = nums.size();
+vector<int> pairSumBrute(vector<int> nums1 , int target1){
+    vector<int> ans1;
+    int n1 = nums1.size();
 
 
     // Check all possible pairs
-    for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
+    for(int i=0; i<n1; i++){
+        for(int j=i+1; j<n1; j++){
 
             // If pair found, return indices
-            if(nums[i]+nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
+            if(nums1[i]+nums1[j] == target1){
+                ans1.push_back(i);
+                ans1.push_back(j);
+                return ans1;
             }
         }
     }
-    return {}; // return empty if no pair found
+    return ans1; // return empty if no pair found
 }
 
-int main(){
-    vector<int> nums = {2,7,11,15};
-    int target = 9;
 
-    vector<int> ans = pairSum(nums,target);
-    
-    // Check before accessing to avoid runtime error
-    if(ans.size() == 2){
-        cout << ans[0] << ", " << ans[1] << endl;
-    } else {
-        cout << "No pair found" << endl;
+// Optimal Approach (Two Pointer) — O(n)
+// NOTE: Array MUST be sorted
+
+vector<int> pairSumOptimal(vector<int> nums2 , int target2){
+    vector<int> ans2;
+    int n2= nums2.size();
+
+    int i = 0; 
+    int j = n2-1;
+    while(i<j){
+        int pairSum = nums2[i]+nums2[j];
+        if(pairSum > target2){
+            j--;
+        }else if(pairSum < target2){
+            i++;
+        }else{
+            ans2.push_back(i);
+            ans2.push_back(j);
+            return ans2;
+        }
     }
+    return ans2;
+}
+
+
+
+int main(){
+    vector<int> nums1 = {2,7,11,15};
+    int target1 = 9;
+
+    // Call brute
+    vector<int> ans1 = pairSumBrute(nums1,target1);
+
+    // Output (Brute)
+    cout << ans1[0] << ", " << ans1[1] << endl;
+     
+
+    vector<int> nums2 = {2, 7, 11, 15};
+    int target2 = 13;
+
+    // call optimal
+    vector<int> ans2 = pairSumOptimal(nums2, target2);
+
+    // output (Optimal)
+    cout << ans2[0] << ", " << ans2[1] << endl;
 
 
     return 0;
